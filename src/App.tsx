53d7b49cf2af5859d7c83e9be13200b5012209b8
Jeong In Jun <jijun897@naver.com>
2025-09-02 00:43:27 +0900

    chore: axios, tanstack query 설정 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Router } from './routes';
+import { queryClient } from './utils';
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 인터셉터 추가 가능
+
+export default apiClient;
