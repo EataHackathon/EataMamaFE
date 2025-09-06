9039865ff72c03700cb45b0bedca956ad1892abf
Jeong In Jun <jijun897@naver.com>
2025-09-06 20:15:34 +0900

    feat: 앱 레이아웃 너비 600px로 설정

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,19 +1,40 @@
 import { Outlet } from 'react-router-dom';
+import styled from '@emotion/styled';
 
-export type LayoutType = 'none' | 'header' | 'navigation';
+export type LayoutType = 'none' | 'left' | 'center';
 
 type AppLayoutProps = {
   layoutType?: LayoutType;
 };
 
-const AppLayout = ({ layoutType = 'none' }: AppLayoutProps) => {
+const AppLayout = ({ layoutType = 'left' }: AppLayoutProps) => {
   return (
-    <div>
-      {layoutType === 'header' && <header>Header</header>}
-      <Outlet />
-      {layoutType === 'navigation' && <footer>Footer</footer>}
-    </div>
+    <Background>
+      <Container>
+        {layoutType !== 'none' && <Header type={layoutType}>Header</Header>}
+        <Outlet />
+        <footer>Footer</footer>
+      </Container>
+    </Background>
   );
 };
 
 export default AppLayout;
+
+const Background = styled.div`
+  width: 100%;
+  min-height: 100dvh;
+`;
+
+const Container = styled.div`
+  max-width: 600px;
+  min-height: 100dvh;
+  background-color: ${({ theme }) => theme.colors.background};
+  margin: 0 auto;
+  padding: 0;
+`;
+
+const Header = styled.header<{ type: LayoutType }>`
+  height: 64px;
+  text-align: ${(props) => props.type};
+`;
