43187bffeecee534ac32a9f6b188c6757c6aca65
Jeong In Jun <jijun897@naver.com>
2025-09-08 23:24:23 +0900

    feat: 음식 검색 페이지 UI 구현

---
+++ src/pages/search/food/components/FoodItem.tsx
@@ -0,0 +1,69 @@
+import { Typography } from '@/components/common';
+import styled from '@emotion/styled';
+
+type FoodItemProps = {
+  title: string;
+  number: number;
+  summary: string;
+  whyGood: string[];
+};
+
+const FoodItem = ({ title, number, summary, whyGood }: FoodItemProps) => {
+  return (
+    <Container>
+      <Box>
+        <Circle>
+          <Typography variant='body2' weight='bold' color='white'>
+            {number}
+          </Typography>
+        </Circle>
+        <Typography variant='body1' weight='bold'>
+          {title}
+        </Typography>
+      </Box>
+      <Typography variant='body3'>{summary}</Typography>
+      <FoodBenefit>
+        <Typography variant='body3' weight='bold'>
+          ✅ 이런 점이 좋아요:
+        </Typography>
+        {whyGood.map((benefit, index) => (
+          <Typography key={index} variant='body3'>
+            {`${index + 1}. ${benefit}`}
+          </Typography>
+        ))}
+      </FoodBenefit>
+    </Container>
+  );
+};
+
+export default FoodItem;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[3]};
+  padding: ${({ theme }) => theme.spacing[4]};
+`;
+
+const Box = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing[2]};
+  align-items: center;
+  margin-bottom: ${({ theme }) => theme.spacing[1]};
+`;
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+const FoodBenefit = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[1]};
+`;
