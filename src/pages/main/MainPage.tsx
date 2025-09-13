06572f73f3939894e2e0d15484ebaef190766a62
Jeong In Jun <jijun897@naver.com>
2025-09-13 23:35:43 +0900

    feat: 메인 페이지 접근시 라우팅 처리 구현

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,12 +1,25 @@
+import { ROUTE_PATH } from '@/routes/paths';
 import { useTitleStore } from '@/stores';
+import { useLoginStore } from '@/stores/login';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const MainPage = () => {
   const { setTitle } = useTitleStore();
+  const { token } = useLoginStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setTitle('Main Page');
   }, [setTitle]);
+
+  useEffect(() => {
+    if (!token) {
+      navigate(ROUTE_PATH.LOGIN);
+    } else {
+      navigate(ROUTE_PATH.DIET);
+    }
+  }, [token, navigate]);
 
   return (
     <div>
